04209230df686d7553031709e75d135145e8748d
Capacap <simon.sorkin@outlook.com>
2024-12-02 16:28:47 +0100

    part_2

--- day_01/main.py
+++ day_01/main.py
@@ -1,7 +1,7 @@
 from input import input
 
 
-def main():
+def part_01():
     left = input[::2]
     right = input[1::2]
     delta = []
@@ -17,5 +17,31 @@ def main():
     print(delta_sum)
 
 
+def part_02():
+    left = input[::2]
+    right = input[1::2]
+
+    number_to_count = {}
+    for number in left:
+        count = 0
+        for other_number in right:
+            if number == other_number:
+                count += 1
+        number_to_count[number] = count
+
+    similarity = []
+    for number in left:
+        try:
+            count = number_to_count[number]
+        except:
+            count = 0
+        similarity.append(number * count)
+
+    score = sum(similarity)
+
+    print(score)
+
+
 if __name__ == "__main__":
-    main()
+    part_01()
+    part_02()
