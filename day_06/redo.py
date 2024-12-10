f6a912b42c4e55daedc31709888a242f1b64cc33
Capacap <simon.sorkin@outlook.com>
2024-12-10 15:08:08 +0100

    hard stuck day 6

--- day_06/redo.py
+++ day_06/redo.py
@@ -1,4 +1,4 @@
-from copy import copy, deepcopy
+from copy import copy
 from typing import Dict, Tuple, Set
 
 
@@ -10,7 +10,13 @@ class Simulation:
         self.guard_dir: Tuple[int, int] = (0, 0)
         self.guard_oob: bool = False
         self.guard_obstructed: bool = False
-        self._turn_map = {(0, -1): (1, 0), (-1, 0): (0, -1), (1, 0): (0, 1), (0, 1): (-1, 0)}
+
+        self._turn_map: Dict[Tuple[int, int], Tuple[int, int]] = {
+            (0, -1): (1, 0),
+            (-1, 0): (0, -1),
+            (1, 0): (0, 1),
+            (0, 1): (-1, 0),
+        }
 
     def progress_simulation(self):
         x = self.guard_pos[0] + self.guard_dir[0]
@@ -21,9 +27,9 @@ class Simulation:
         if next_pos in self.obstacles:
             self.guard_obstructed = True
             self.guard_dir = self._turn_map[self.guard_dir]
-            x = self.guard_pos[0] + self.guard_dir[0]
-            y = self.guard_pos[1] + self.guard_dir[1]
 
+        x = self.guard_pos[0] + self.guard_dir[0]
+        y = self.guard_pos[1] + self.guard_dir[1]
         self.guard_pos = (x, y)
 
         self.guard_oob = False
