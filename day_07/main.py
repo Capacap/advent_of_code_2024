a9f5962887ca21019f1feb9f4d5f4e757586a4a6
Capacap <simon.sorkin@outlook.com>
2024-12-10 17:38:23 +0100

    day 7

---
+++ day_07/main.py
@@ -0,0 +1,63 @@
+from copy import copy
+from typing import Dict, Tuple, Set
+from itertools import product
+
+def add(a, b):
+    return a + b
+
+def mul(a, b):
+    return a * b
+
+def concat(a, b):
+    return int(str(a) + str(b))
+
+def main() -> None:
+    test_values = []
+    test_series = []
+    with open("./day_07/input.txt", "r") as file:
+        for line in file:
+            series = list(map(int, line.strip().replace(":", "").split()))
+            test_values.append(series[0])
+            test_series.append(series[1:])
+
+    num_to_op_map = { 0: add, 1: mul, 2:concat}
+
+    test_validity = []
+    for value, series in zip(test_values, test_series):
+        for op_combo in set(product([0, 1], repeat=len(series) - 1)):
+            valid = False
+
+            lhs = series[0]
+            for i, op in enumerate(op_combo):
+                rhs = series[i + 1]
+                lhs = num_to_op_map[op](lhs, rhs)
+
+            if lhs == value:
+                valid = True
+                break
+
+        test_validity.append(valid)
+
+    print(sum([v for i, v in enumerate(test_values) if test_validity[i]])) # Part 1 - 1708857123053
+
+    test_validity = []
+    for value, series in zip(test_values, test_series):
+        for op_combo in set(product([0, 1, 2], repeat=len(series) - 1)):
+            valid = False
+
+            lhs = series[0]
+            for i, op in enumerate(op_combo):
+                rhs = series[i + 1]
+                lhs = num_to_op_map[op](lhs, rhs)
+
+            if lhs == value:
+                valid = True
+                break
+
+        test_validity.append(valid)
+
+    print(sum([v for i, v in enumerate(test_values) if test_validity[i]])) # Part 2 - 189207836795655
+
+
+if __name__ == "__main__":
+    main()
